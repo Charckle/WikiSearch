47a9103ef5a9dd76d996a9d8e6f5ce6855265591
charckle <andrej.zubin@email.com>
2019-12-01 19:29:16 +0100

    before changing the frames for the canvas i nsearch, from pack to grid

--- frames/searchscreen.py
+++ frames/searchscreen.py
@@ -27,6 +27,22 @@ class SearchScreen(ttk.Frame):
         #separator
         ttk.Separator(self, orient="horizontal").pack(side="top", fill ="x", pady=5)    
         
+        #canvas
+        self.canvas = tk.Canvas(self)
+        self.scrollable_frame = ttk.Frame(self.canvas)
+        
+        self.scrollable_window = self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
+        
+        def configure_scroll_region(event):
+            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+            
+        self.scrollable_frame.bind("<Configure>", configure_scroll_region)
+        
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        
+        self.scrollbar.grid(row=0, column=1, sticky="NS")
+        self.canvas.grid(row=0, column=0, sticky="NSEW")
         #bind enter
         #controller.bind("<Return>", self.exit)
         #controller.bind("<KP_Enter>", self.exit)

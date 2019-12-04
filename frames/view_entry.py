3ac08107cda60c765d59c8e5db209648568c4ac0
charckle <andrej.zubin@email.com>
2019-12-04 18:05:51 +0100

    finished edit and save of entries

--- frames/view_entry.py
+++ frames/view_entry.py
@@ -13,23 +13,96 @@ class ViewEntry(ttk.Frame):
         self.rowconfigure(0, weight=1)
      
         #variables
-        entry_filename= tk.StringVar()
-        entry_filename.set("data//bananaaassa.txt")
-        data = tk.StringVar()        
-     
+        self.entry_filename= tk.StringVar()
+        self.entry_filename.set("data//bananaaassa.txt")
+        self.data = tk.StringVar()
+        
         #entry text  
-        entry_text = tk.Text(self)
-        #populate text
-        try:
-            with open(f'{entry_filename.get()}', 'r') as file:
-                data.set(file.read())
-        except:
-            data.set("No file selected")
-            
-        entry_text.grid(row=0, column=0, sticky="NESW", padx=(5,5), pady=(5,5))
-        entry_text.insert("1.0", data.get())    #the 1 means the line it will insert the text, the 0 is the character. Its weird, but OK
-        entry_text["state"] = "disable" 
+        self.entry_text = tk.Text(self)
+        self.entry_text.grid(row=0, column=0, sticky="NESW", padx=(5,5), pady=(5,5))
+        #get data from file and populate
+        self.populate_from_file()        
+        
         #scroll widget
-        text_scroll = ttk.Scrollbar(self, orient="vertical", command=entry_text.yview)             #"text.yview" links the scroll widget to the y-axis of the text widget
+        text_scroll = ttk.Scrollbar(self, orient="vertical", command=self.entry_text.yview)             #"text.yview" links the scroll widget to the y-axis of the text widget
         text_scroll.grid(row=0, column=1, sticky="ns")
-        entry_text["yscrollcommand"] = text_scroll.set               
+        self.entry_text["yscrollcommand"] = text_scroll.set               
+        
+        #button_container
+        self.button_container = ttk.Frame(self)
+        self.button_container.columnconfigure(0, weight=1)
+        self.button_container.grid(columnspan=2, row=1, column=0, sticky="NESW")        
+        
+        #Edit button populate
+        self.view_mode()
+    
+
+    def populate_from_file(self):
+        try:
+            with open(f'{self.entry_filename.get()}', 'r') as file:
+                self.data.set(file.read())
+        except:
+            self.data.set("No file selected")
+        
+        self.entry_text.insert("1.0", self.data.get())    #the 1 means the line it will insert the text, the 0 is the character. Its weird, but OK
+        self.entry_text["state"] = "disable"         
+    
+    def save_entry(self):
+        self.clear()
+        
+        with open(f'{self.entry_filename.get()}', 'w') as file:
+            file.write(self.entry_text.get("1.0",tk.END))    
+    
+        self.entry_text["state"] = "disable"
+        
+        self.view_mode()
+        
+        
+    def edit_mode(self):
+        self.clear()
+        self.entry_text["state"] = "normal"   
+        
+        #Save button
+        self.edit_button = ttk.Button(
+                    self.button_container,
+                    text="Save entry",
+                    command=self.save_entry,
+                    cursor="hand2"
+                )
+        
+        self.edit_button.grid(columnspan=1, row=0, column=0, sticky="NESW", padx=(5,5), pady=(5,5))
+        
+        #separator
+        ttk.Separator(self.button_container, orient="horizontal").grid(columnspan=2, row=1, column=0, sticky="EW", padx=(5,5), pady=(5,5))         
+        
+        #Cancel button
+        self.edit_button = ttk.Button(
+                    self.button_container,
+                    text="Cancel editing entry",
+                    command=self.view_mode,
+                    cursor="hand2"
+                )
+        
+        self.edit_button.grid(columnspan=1, row=2, column=0, sticky="NESW", padx=(5,5), pady=(5,5))
+    
+    def view_mode(self):
+        self.clear()
+        
+        #cancel made changes
+        self.entry_text.delete('1.0', tk.END)
+        self.populate_from_file()
+        
+        #Edit button
+        self.edit_button = ttk.Button(
+                    self.button_container,
+                    text="Edit entry",
+                    command=self.edit_mode,
+                    cursor="hand2"
+                )
+        
+        self.edit_button.grid(columnspan=1, row=0, column=0, sticky="NESW", padx=(5,5), pady=(5,5))     
+    
+    def clear(self):
+        list = self.button_container.grid_slaves()
+        for l in list:
+            l.destroy()    
